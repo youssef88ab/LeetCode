48c65ab80994d361bbe0fe98837be3a251ee6e2c
YOUSSEF ABOU ELJIHAD <82283823+youssef88ab@users.noreply.github.com>
2024-08-21 13:26:36 +0100

    Time: 54 ms (37.68%) | Memory: 12.3 MB (7.01%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ranlist = list()
+        for i in ransomNote :
+            ranlist.append(i)
+        
+        maglist = list()
+        for j in magazine :
+            maglist.append(j)
+
+        dup = list()
+
+        for ch in ranlist : 
+            if ch in maglist :
+                maglist.remove(ch)
+                dup.append(ch)
+            elif ch not in maglist :
+                return False 
+        return True
