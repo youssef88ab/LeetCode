16ac7b15696cc9fa7d3bb8d3313427813dd09d03
YOUSSEF ABOU ELJIHAD <82283823+youssef88ab@users.noreply.github.com>
2024-09-23 22:50:47 +0100

    Time: 0 ms (100.00%) | Memory: 44.6 MB (44.10%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        if (n == 0) { return nums ;}
+
+        int[] ans = new int[nums.length];
+        for (int i = 0 , j = 0 ; i < ans.length ; i+=2 , j++) {
+            ans[i] = nums[j];   
+        }
+
+        for (int i = 1 , j = n ; i < ans.length ; i+=2 , j++) {
+            ans[i] = nums[j];
+        }
+
+        return ans ;
+        
+    }
+}
