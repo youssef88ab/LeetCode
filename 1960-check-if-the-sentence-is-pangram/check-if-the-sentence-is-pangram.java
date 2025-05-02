6875e93e000998f43a3a3780ecb5f2d581332297
YOUSSEF ABOU ELJIHAD <82283823+youssef88ab@users.noreply.github.com>
2025-05-02 22:26:33 +0100

    Time: 0 ms (100.00%) | Memory: 41.9 MB (30.95%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        for (char c = 'a' ; c <= 'z' ; c++) {
+            if (sentence.indexOf(c) == -1) {
+                return false ; 
+            }
+        }
+        return true ;
+    }
+}
