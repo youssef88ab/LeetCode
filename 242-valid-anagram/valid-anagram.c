e23f7401f99bf0ceecd4cd734ba1768841a9913c
YOUSSEF ABOU ELJIHAD <yousef.aboueljihad@gmail.com>
2025-10-15 23:54:21 +0100

    Time: 12 ms (19.76%) | Memory: 8.3 MB (30.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.c
+++ 242-valid-anagram/valid-anagram.c
@@ -5,6 +5,15 @@ int compare(const void *a, const void *b) {
 }
 
 bool isAnagram(char* s, char* t) {
+   int sSum  = 0;
+   int tSum = 0;
+   for (char *c=s;*c!='\0';c++){
+     sSum = sSum + *c;
+   }
+   for (char *c=t;*c!='\0';c++){
+     tSum = tSum + *c;
+   }
+   if(tSum!=sSum) return false;
    int sTaille = strlen(s);
    int tTaille = strlen(t);
    
